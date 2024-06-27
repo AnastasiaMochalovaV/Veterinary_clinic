abb2a3a1d3042e2863098caa45c123ccac046f86
Nastya <nastya.m04alova@yandex.ru>
2024-06-27 22:40:02 +0300

    windows home, login, registr, client, doctor

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/HomePage.fxml"));
+//        Parent root = loader.load();
+
+        Parent root = FXMLLoader.load(getClass().getResource("View/HomePage.fxml"));
+
+//        Model model = new Model();
+//        controller.initModel(model, primaryStage);
+//        model.getAllBikeModels();
+
+        primaryStage.setTitle("Veterinary clinic");
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.show();
     }
 }
